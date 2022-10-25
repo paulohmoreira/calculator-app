32d50715c8518496d63fb4a8ee97bdbc82c2e0cb
paulohmoreira <phmoreira37@gmail.com>
2022-10-25 19:31:39 -0300

    adicionado função mudar tema
    Ainda está com bugs:
    Não consegui sobrescrever a cor do botão que faz a função da troca, ele está com mesma cor da div.
    
    Também não consegui salvar a opção de tema selecionado no local storage

---
+++ assets/js/theme-toggle.js
@@ -0,0 +1,50 @@
+const btnContainer = document.getElementById("theme-toggle");
+const btns = btnContainer.getElementsByClassName("btn-box");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    const current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme(theme) {
+  if (theme === 'theme-3') {
+    document.getElementById("equals-button").style.color = "hsl(198, 20%, 13%)";
+    document.querySelector(".topo").style.color = "hsl(52, 100%, 62%)";
+    document.getElementById("operacoes").style.color = "hsl(52, 100%, 62%)";
+  }
+  if (theme === 'theme-2') {
+    document.getElementById("equals-button").style.color = "#FFF";
+    document.querySelector(".topo").style.color = "hsl(221, 14%, 31%)";
+    document.getElementById("operacoes").style.color = "hsl(221, 14%, 31%)";
+  }
+  if (theme === 'theme-1') {
+    document.querySelector(".topo").style.color = "#FFF";
+    document.getElementById("operacoes").style.color = "#FFF";
+    document.getElementById("equals-button").style.color = "#FFF";
+  }
+  setTheme(theme);
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+  if (localStorage.getItem('theme') === 'dark') {
+    setTheme('theme-1');
+  } else {
+    setTheme('theme-2');
+  }
+})();
