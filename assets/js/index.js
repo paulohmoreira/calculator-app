dff696dd7ce7e4da5f553e1ee3216591cb49f7a2
paulohmoreira <phmoreira37@gmail.com>
2022-10-25 11:38:31 -0300

    Refatorado todo código JS da calculadora

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,87 +1,129 @@
-const tela = document.getElementById('tela');
-const btn = Array.from(document.getElementsByClassName('btn'));
-const clear = document.getElementById('reset-button');
-const del = document.getElementById('del');
-const equals = document.getElementById('equals-button');
+const textoOperadorAnterior = document.querySelector("#operacao-anterior");
+const textoOperadorAtual = document.querySelector("#operacao-atual");
+const botoes = document.querySelectorAll(".calculadora button");
 
-
-
-// Adicionar numeros e operadores no display da calculadora
-function adcTela(e) {
-  const ultimoDigito = tela.innerText.slice(-1);
-  const isUltimoDigitoOperador = (ultimoDigito === '+' || ultimoDigito === '-' || ultimoDigito === 'x' || ultimoDigito == '/');
-  const isBtnOperador = (btn.innerText === '+' || btn.innerText === '-' || btn.innerText === 'x' || btn.innerText == '/');
-  const isPonto = (tela.innerText.includes('.') && btn.innerText === '.');
-
-  if (isBtnOperador && isUltimoDigitoOperador) {
-    return
-  } else if (isPonto) {
-    return
+class Calculadora {
+  constructor(textoOperadorAnterior, textoOperadorAtual) {
+    this.textoOperadorAnterior = textoOperadorAnterior;
+    this.textoOperadorAtual = textoOperadorAtual;
+    this.operacaoAtual = "";
   }
-  tela.innerText += e.target.innerText;
-}
 
-btn.map(btn => {
-  btn.addEventListener('click', (e) => {
-    adcTela(e);
-  })
-})
+  // adiciona digitos à tela da calculadora
+  adicionaDigitos(digito) {
+    // Checar se o número já tem um ponto
+    if (digito === "." && this.textoOperadorAtual.innerText.includes(".")) {
+      return;
+    }
 
-// Limpar display da calculadora
-function limpaTela() {
-  tela.innerText = '';
-}
-clear.addEventListener('click', () => {
-  limpaTela();
-})
-
-// Função delete button DEL
-del.addEventListener('click', () => {
-  if (tela.innerText) {
-    tela.innerText = tela.innerText.slice(0, -1);
+    this.operacaoAtual = digito;
+    this.atualizaTela();
   }
-})
 
+  // processa todos as operações da calculadora
+  processaOperacao(operacao) {
+    // Checa se o valor atual está vazio
+    if (this.textoOperadorAtual.innerText === "" && operacao !== "RESET") {
+      // Muda operação
+      if (this.textoOperadorAnterior.innerText !== "") {
+        this.mudaOperacao(operacao);
+      }
+      return;
+    }
 
-// Função do botão "igual"
-equals.addEventListener('click', () => {
-  operacaoMatematica();
-})
+    // Pega valor atual e anterior
+    let valorOperacao;
+    let anterior = +this.textoOperadorAnterior.innerText.split(" ")[0];
+    let atual = +this.textoOperadorAtual.innerText;
 
-// Operações matemáticas
-function operacaoMatematica() {
-  const operadoresMatematicos = ["x", "-", "+", "/"];
-  const operacaoCapturada = tela.innerText;
-  let operadorClicado = '';
-  let n1 = '';
-  let n2 = '';
-  let resultado = undefined;
+    switch (operacao) {
+      case "+":
+        valorOperacao = anterior + atual;
+        this.atualizaTela(valorOperacao, operacao, atual, anterior);
+        break;
+      case "-":
+        valorOperacao = anterior - atual;
+        this.atualizaTela(valorOperacao, operacao, atual, anterior);
+        break;
+      case "x":
+        valorOperacao = anterior * atual;
+        this.atualizaTela(valorOperacao, operacao, atual, anterior);
+        break;
+      case "/":
+        valorOperacao = anterior / atual;
+        this.atualizaTela(valorOperacao, operacao, atual, anterior);
+        break;
+      case "DEL":
+        this.processaBtnDel();
+        break;
+      case "RESET":
+        this.processaBtnReset();
+        break;
+      case "=":
+        this.processaBtnIgual();
+        break;
+      default:
+        return;
+    }
 
-  for (let i = 0; i < operadoresMatematicos.length; i++) {
-    if (operacaoCapturada.includes(operadoresMatematicos[i])) {
-      const operacaoFinal = operacaoCapturada.split(operadoresMatematicos[i]);
-      operadorClicado = operadoresMatematicos[i];
-      n1 = operacaoFinal[0];
-      n2 = operacaoFinal[1];
+  }
+
+  // Muda os valores na tela da calculadora
+  atualizaTela(valorOperacao = null, operacao = null, atual = null, anterior = null) {
+    if (valorOperacao === null) {
+      // Adiciona número ao operador atual
+      this.textoOperadorAtual.innerText += this.operacaoAtual;
+    } else {
+      // checa se o valor é 0, se for apenas adiciona o valor atual
+      if (anterior === 0) {
+        valorOperacao = atual;
+      }
+      // Muda o valor atual para o anterior
+      this.textoOperadorAnterior.innerText = `${valorOperacao} ${operacao}`;
+      this.textoOperadorAtual.innerText = "";
     }
   }
 
-  switch (operadorClicado) {
-    case '+':
-      resultado = Number(n1) + Number(n2)
-      tela.innerText = resultado;
-      break;
-    case '-':
-      resultado = Number(n1) - Number(n2)
-      tela.innerText = resultado;
-      break;
-    case '/':
-      resultado = Number(n1) / Number(n2)
-      tela.innerText = resultado;
-      break;
-    case 'x':
-      resultado = Number(n1) * Number(n2)
-      tela.innerText = resultado;
-      break;
+  // Muda o operador matemático
+  mudaOperacao(operacao) {
+    const operadorMatematico = ["x", "-", "+", "/"];
+
+    if (!operadorMatematico.includes(operacao)) {
+      return;
+    }
+
+    this.textoOperadorAnterior.innerText = this.textoOperadorAnterior.innerText.slice(0, -1) + operacao;
   }
+
+  // Deleta um digito
+  processaBtnDel() {
+    this.textoOperadorAtual.innerText = this.textoOperadorAtual.innerText.slice(0, -1);
+  }
+
+  // Limpa toda a tela
+  processaBtnReset() {
+    this.textoOperadorAtual.innerText = "";
+    this.textoOperadorAnterior.innerText = "";
+  }
+
+  // Process an operation
+  processaBtnIgual() {
+    let operacao = this.textoOperadorAnterior.innerText.split(" ")[1];
+    this.processaOperacao(operacao);
+  }
+
 }
+
+const calc = new Calculadora(textoOperadorAnterior, textoOperadorAtual);
+
+botoes.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const valor = e.target.innerText;
+
+    if (+valor >= 0 || valor === ".") {
+      calc.adicionaDigitos(valor);
+    } else {
+      calc.processaOperacao(valor);
+    }
+  });
+});
