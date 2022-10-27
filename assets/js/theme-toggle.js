66ed650ee19acb5744f55b7cc86d88d4987d1aa1
paulohmoreira <phmoreira37@gmail.com>
2022-10-27 11:33:16 -0300

    correção feita no arquivo theme-toggle.js
    Essa correção fez a função de salvar o tema escolhido pelo usuário no localstorage funcionar

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -5,23 +5,10 @@ const equals = document.getElementById("equals-button");
 const topo = document.querySelector(".topo");
 const telaOperacoes = document.getElementById("operacoes")
 
-// Loop through the buttons and add the active class to the current/clicked button
+// Loop through the buttons
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    const current = document.getElementsByClassName("active");
-
-    // If there's no active class
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace("active", "cor-fundo");
-    }
-
-    // Add the active class to the current/clicked button
-    this.className = this.className.replace("cor-fundo", "active");
-
-    // Alterando plano de fundo dos botões não ativos
-    for (let j = 0; j < btns.length; j++) {
-      if(i != j) btns[j].className = btns[j].className.replace("active", "cor-fundo");
-    }
+    setActiveButton(i, ...btns);
   });
 }
 
@@ -30,7 +17,8 @@ function setTheme(themeName) {
   localStorage.setItem('theme', themeName);
   document.documentElement.className = themeName;
 }
-// function to toggle between light and dark theme
+
+// function to toggle between theme-1, theme-1 and theme-3
 function toggleTheme(theme) {
   if (theme === 'theme-3') {
     equals.style.color = "var(--text-dark)";
@@ -49,11 +37,35 @@ function toggleTheme(theme) {
   }
   setTheme(theme);
 }
+
+//function to add the active class to the current/clicked button
+function setActiveButton(i, ...btns) {
+  const current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace("active", "cor-fundo");
+    }
+
+    // Add the active class to the current/clicked button
+    btns[i].className = btns[i].className.replace("cor-fundo", "active");
+
+    // Alterando plano de fundo dos botões não ativos
+    for (let j = 0; j < btns.length; j++) {
+      if(i != j) btns[j].className = btns[j].className.replace("active", "cor-fundo");
+    }
+}
+
 // Immediately invoked function to set the theme on initial load
 (function () {
-  if (localStorage.getItem('theme') === 'dark') {
-    setTheme('theme-1');
+  if (localStorage.getItem('theme') === 'theme-1') {
+    toggleTheme('theme-1');
+    setActiveButton(0, ...btns);
+  } else if (localStorage.getItem('theme') === 'theme-2') {
+    toggleTheme('theme-2');
+    setActiveButton(1, ...btns);
   } else {
-    setTheme('theme-2');
+    toggleTheme('theme-3');
+    setActiveButton(2, ...btns);
   }
 })();
